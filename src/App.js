d6f6cc9af541bde9cab145bf0b4f70b461285863
Khusboo Kumari <36759104+khush611@users.noreply.github.com>
2020-10-21 17:12:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NewJSFeatures from "./NewJSFeatures";
+
+function App() {
+  return (
+    <div>
+      <NewJSFeatures />
+    </div>
+  );
+}
+
+export default App;
